d361e4f5440b0466a56bcc5aebd7abed7640b1d9
Witwork App <witwork.digital@gmail.com>
2023-02-15 00:11:42 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,16 +14,17 @@ let package = Package(
     dependencies: [
         .package( url: "https://github.com/lmirosevic/GBDeviceInfo.git", branch: "master"),
         .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk.git", branch: "master"),
-        .package( url: "https://github.com/google/GoogleSignIn-iOS.git", branch: "main")
+        .package(name: "GoogleSignIn", url: "https://github.com/google/GoogleSignIn-iOS.git", branch: "main")
     ],
     targets: [
         .target(
             name: "FIRManagerTarget",
             dependencies: [
                 .product(name: "FirebaseAuth", package: "Firebase"),
-                .product(name: "FirebaseCore", package: "Firebase"),
                 .product(name: "FirebaseFirestore", package: "Firebase"),
-                .product(name: "GoogleSignIn", package: "GoogleSignIn-iOS"),
+                .product(name: "FirebaseCrashlytics", package: "Firebase"),
+                .product(name: "FirebaseAnalytics", package: "Firebase"),
+                .product(name: "GoogleSignIn", package: "GoogleSignIn"),
                 .product(name: "GBDeviceInfo", package: "GBDeviceInfo")
             ],
             path: "FIRManager",
